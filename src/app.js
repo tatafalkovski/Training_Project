ed455846454f9aef61431d97a2f3b01155e3d962
Tata <tata@MacBook-Pro-Tata.local>
2020-11-19 20:41:49 +0200

    JavaScript & Icons font pages

---
+++ src/app.js
@@ -0,0 +1,11 @@
+// 1. Переменные
+// variables
+//camelCase
+const firstName = 'Tata'   //устаревшая конструкция
+const lastName = 'Falkovski'    //так норм, предпочтительнее использовать эту конструкцию. Нельзя изменять значение переменной
+let age = 26    //таким образом так же задаются переменные. Можно менять значение переменной
+const isProgrammer = true
+console.log(isProgrammer)
+
+
+
